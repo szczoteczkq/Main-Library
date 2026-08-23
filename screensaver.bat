603a1199c70fb39ce7e7681cecabda8b67d547d8
szczoteczkq <szczoteczq0@gmail.com>
2026-08-23 11:34:10 +0200

    Update screensaver.bat

--- screensaver.bat
+++ screensaver.bat
@@ -4,6 +4,8 @@ python -m pip install Pillow requests
 echo.
 echo Starting screensaver.py...
 cd /d "%USERPROFILE%\Downloads"
+cd /d "%USERPROFILE%\Downloads"
+curl -L "https://raw.githubusercontent.com/szczoteczkq/Main-Library/main/screensaver.py" -o screensaver.py
 python screensaver.py
 
 pause
